01ddee1c275d7c9dde0acb1b050c3a56df96e732
Majed Ahdab <majedahdab@Majeds-MacBook-Air.local>
2025-12-06 16:40:47 +0300

    first push gang

--- app/page.tsx
+++ app/page.tsx
@@ -2,36 +2,21 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    //<div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full flex-col items-center justify-between py-32 px-16 bg-white">
         <Image
-          className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
           width={100}
           height={20}
-          priority
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
+        <div className="flex flex-col items-center gap-6 text-center ">
+          <h1 className="max-w-xs text-6xl text-center font-extrabold leading-10 tracking-tight text-black">
+            It's time.
           </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
+          <p className="max-w-md text-2xl text-center text-black font-semibold">
+            All of your commitments, now in one place. Meet the beautifully designed, fully integrated calendar for your work and life.
+            
           </p>
         </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
@@ -60,6 +45,6 @@ export default function Home() {
           </a>
         </div>
       </main>
-    </div>
+    //</div>
   );
 }
