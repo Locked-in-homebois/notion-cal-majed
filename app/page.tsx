425313936372d7e5122d93f1a55097a8633877bf
Majed Ahdab <majedahdab@Majeds-MacBook-Air.local>
2025-12-06 22:02:42 +0300

    YOUR MESSAGE

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import Image from "next/image";
 import Hero from '@/components/hero';
+import Video from'@/components/video';
 export default function Home() {
   return (
       <main className="flex min-h-screen w-full flex-col items-center bg-white">
         <Hero />
-        
+        <Video />
       </main>
     
   );
---
+++ components/video.tsx
@@ -0,0 +1,12 @@
+const Video = () => {
+return(
+    <div className=" bg-gray-100 rounded-xl w-5xl h-[575px] mb-6 px-10 text-black text-xl text-center">
+        video here gang
+    </div>
+
+
+
+
+    )
+}
+export default Video;
