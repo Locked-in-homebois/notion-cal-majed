9e41dfc59b195548cb5ae5f80e306a86c907f6cf
Majed Ahdab <majedahdab@Majeds-MacBook-Air.local>
2025-12-07 20:44:37 +0300

    did the quatro

--- components/quatroGang.tsx
+++ components/quatroGang.tsx
@@ -1,17 +1,17 @@
 import Image from "next/image";
 const Quatro = () => {
 return(
-    <div className="grid-cols-2 grid-rows-2">
-        <div className=" max-h-[640px] w-fit">
-            <div className=" bg-gray-100 grid-flow-col rounded-xl w-xl h-[630px] mb-5  py-7 text-black text-xl">
+    <div className="">
+        <div className=" flex ">
+            <div className=" bg-gray-100 rounded-xl w-[500] h-[535px] mb-6 mx-3 py-7 text-black text-xl ">
             </div>
-            <div className=" bg-gray-100 grid-flow-col rounded-xl w-xl h-[630px] mb-5  py-7 text-black text-xl">
+            <div className=" bg-gray-100 rounded-xl w-[500] h-[535px] mb-6 mx-3 py-7 text-black text-xl ">
             </div>
         </div>
-        <div>
-            <div className=" bg-gray-100 grid-flow-col rounded-xl w-xl h-[630px] mb-5  py-7 text-black text-xl">
+        <div className=" flex ">
+            <div className=" bg-gray-100 rounded-xl w-[500] h-[535px] mb-1 mx-3 py-7 text-black text-xl">
             </div>
-            <div className=" bg-gray-100 grid-flow-col rounded-xl w-xl h-[630px] mb-5  py-7 text-black text-xl">
+            <div className=" bg-gray-100 rounded-xl w-[500] h-[535px] mb-1 mx-3 py-7 text-black text-xl">
             </div>
         </div>
     </div>
