50fe06a2332ae3ce4ae39923935e8f869c34e20e
Majed Ahdab <majedahdab@192.168.1.195>
2025-12-08 11:23:09 +0300

    updated link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     return (
     <nav className="fixed top-0 flex w-full flex-col pb-12 h-[66px] bg-white shadow-xs">
         <div className="items-center text-black px-[25px] py-[18px]">
-            <Link href='/'>
+            <Link href='https://youtu.be/xvFZjo5PgG0?si=BnbvMVhfynKkNWpz'>
             <Image src="/notion-icon.svg" alt="notion logo" width={29} height={30}>
             </Image>
             </Link>
