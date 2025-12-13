f9008268a270f1eaf4d733ef065b8296788aa869
Majed Ahdab <majedahdab@192.168.1.131>
2025-12-13 14:01:37 +0300

    small changes cleaner QA

--- components/Q&A.tsx
+++ components/Q&A.tsx
@@ -16,74 +16,47 @@ const QA = () => {
         setQA3IsOpen(!QA3isOpen);
     };
     return (
-        <div className="grid grid-rows-3 grid-cols-1 h-fit w-[50%] mb-[155px] text-left items-center">
-            <div className=" bg-white   text-black">
+        <div className="grid grid-rows-3 grid-cols-1 h-fit w-[50%] text-left items-center text-black">
+            <div onClick={handleQA1}>
                 <h1 className="text-2xl ">
                     Which calendar provider(s) is Notion Calendar compatible
                     with?
                 </h1>
                 <div>
-                    <PlusIcon
-                        text-black
-                        onClick={handleQA1}
-                        className="place-items-end justify-self-end"
-                    />
+                    <PlusIcon className="justify-self-end" />
                 </div>
-                <div
-                    className={
-                        QA1isOpen
-                            ? " place-items-end items-center h-auto bg-white "
-                            : "hidden"
-                    }
-                >
+                <div className={QA1isOpen ? "  h-auto " : "hidden"}>
                     Currently, Notion Calendar integrates and syncs with Google
                     Calendar accounts and Apple iCloud-synced Calendars. Adding
                     support for other calendar providers such as Outlook is on
                     our roadmap.
                 </div>
             </div>
-            <div className=" bg-white border-t border-b py-[15px] border-gray-200  text-black">
+            <div
+                onClick={handleQA2}
+                className=" border-t border-b py-[15px] border-gray-200 "
+            >
                 <h1 className="text-2xl">
                     Is Notion Calendar available on mobile devices?
                 </h1>
                 <div>
-                    <PlusIcon
-                        text-black
-                        onClick={handleQA2}
-                        className="place-items-end justify-self-end"
-                    />
+                    <PlusIcon className=" justify-self-end" />
                 </div>
-                <div
-                    className={
-                        QA2isOpen
-                            ? " place-items-end items-center h-auto bg-white "
-                            : "hidden"
-                    }
-                >
+                <div className={QA2isOpen ? "  h-auto " : "hidden"}>
                     Yes, Notion Calendar is available for iPhone and Android
                     devices. We know some users are looking for an optimized
                     version for tablet devices, such as iPad, and are working to
                     make Notion Calendar the best experience on all platforms.
                 </div>
             </div>
-            <div className=" bg-white border-t border-b py-[15px] border-gray-200  text-black">
+            <div onClick={handleQA3}>
                 <h1 className="text-2xl ">
                     Does Notion Calendar bring Google Calendar sync to Notion?
                 </h1>
                 <div>
-                    <PlusIcon
-                        text-black
-                        onClick={handleQA3}
-                        className="place-items-end justify-self-end"
-                    />
+                    <PlusIcon className="justify-self-end" />
                 </div>
-                <div
-                    className={
-                        QA3isOpen
-                            ? " place-items-end items-center h-auto bg-white "
-                            : "hidden"
-                    }
-                >
+                <div className={QA3isOpen ? "h-auto " : "hidden"}>
                     Notion Calendar allows you to view your Notion database
                     items alongside your Google Calendar events. This offers a
                     streamlined way to see project timelines and task due dates
